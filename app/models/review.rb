4fdb0bd4fc160b8f00de3f6b5d8ad3a1c200e1a3
oidaryou <oidaryou0311@gmail.com>
2021-10-25 03:39:27 +0000

    [Fix] Layout

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   belongs_to :customer
   belongs_to :book
   has_many :likes, dependent: :destroy
+  validates :review,
+    length: { minimum: 1, maximum: 150 }
 end
