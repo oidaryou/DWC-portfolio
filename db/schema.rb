1a3fe7a547f85cb5a548d19165f3de2dc9b0d0ec
oidaryou <oidaryou0311@gmail.com>
2022-02-09 05:55:21 +0000

    [Fix] Layout

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2021_11_02_091136) do
     t.string "encrypted_password", default: "", null: false
     t.string "name"
     t.string "profile_image_id"
-    t.boolean "is_deleted", default: false, null: false
+    t.boolean "is_deleted", default: true, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
